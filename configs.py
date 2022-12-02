ad35ad4270efe68611ab07b946e6e4f3643af88b
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 10:49:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,12 +3,12 @@ from os import path, getenv
 SUDO = "5171347305"
 
 class Config:
-    API_ID = int(getenv("API_ID", "14055090"))
-    API_HASH = getenv("API_HASH", "a46f7b439d0afa45b7a69fc450f754e9")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:AAFk0OhEkbFuUMrSSs6dfLdBYhxUI0c9Pt4")
+    API_ID = int(getenv("API_ID", "140090"))
+    API_HASH = getenv("API_HASH", "a46f7b439d0f754e9")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:dBYhxUI0c9Pt4")
     FSUB = getenv("FSUB", "EmoBotDevolopers")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbs=true&w=majority")
     
 cfg = Config()
