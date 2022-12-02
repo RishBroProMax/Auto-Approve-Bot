e5861bde94d2cd04f9a0ebbd4ede79c932acc125
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 10:48:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:AAFk0OhEkbFuUMrSSs6dfLdBYhxUI0c9Pt4")
     FSUB = getenv("FSUB", "EmoBotDevolopers")
     CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO","5171347305").split()))
+    SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     
 cfg = Config()
